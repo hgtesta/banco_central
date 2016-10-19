1a014fc9156c8c683a81d0e5892c27ff985b31c6
Henrique G. Testa <hgtesta@gmail.com>
2016-10-19 19:00:13 -0200

    Fix missing mocha gem, we don't need it anymore

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,7 +4,6 @@ require 'banco_central'
 require 'active_support'
 require 'active_support/test_case'
 require 'minitest/autorun'
-require 'mocha/mini_test'
 require "minitest/reporters"
 
 Minitest::Reporters.use!
